97bb695cb83176bee93236b1c0ede004903c709b
Rakyun <73707080+Rakyun@users.noreply.github.com>
2021-04-29 11:37:56 +0700

    Add files via upload

---
+++ mouse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import skywriter
+import signal
+import autopy
+import uinput
+import time
+some_value = 0
+
+mouse = uinput.Device([uinput.REL_X, uinput.REL_Y, uinput.BTN_LEFT])
+x = 0
+y = 0
+@skywriter.move()
+def move(x, y, z):
+  #print( x, y, z )
+  x = (x) * 8
+  y = (y) * 8
+
+  x = int(x)
+  y = int(y)
+
+  mouse.emit(uinput.REL_X, x)
+  mouse.emit(uinput.REL_Y, y)
+  time.sleep(0.01)
+  #print( int(x), int(y) )
+
+
+
+
+signal.pause()
